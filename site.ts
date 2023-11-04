a5c9eb16c2468136c127b2ac425d6cb48647e7db
Akayy <techakayy@gmail.com>
2023-11-04 15:31:58 +1100

    01 Scaffolded Nuxt UI blank project

---
+++ site.ts
@@ -0,0 +1,31 @@
+export default {
+  name: 'NuCommerce',
+  description:
+    'Vue Designer Nuxt Tailwind CSS / Nuxt UI - Quick start template',
+  logo: 'i-fluent-emoji:shopping-bags',
+  author: 'Pinegrow',
+  url: 'https://pg-nuxtui-shadcn-vue-x-clone.netlify.app',
+  github: 'https://github.com/pinegrow/pg-nuxtui-shadcn-vue-x-clone',
+  defaultLocale: 'en', // default
+  identity: {
+    type: 'Organization',
+  } as any,
+  twitter: '@vuedesigner',
+  trailingSlash: false, // default
+  titleSeparator: '|', // default
+  nav: [
+    { text: 'Home', link: '/', type: 'primary', icon: 'i-mdi-home' },
+    // {
+    //   text: 'Contact Us',
+    //   link: '/contact-us',
+    //   type: 'secondary',
+    //   icon: 'i-material-symbols-add-call',
+    // },
+    // {
+    //   text: 'Help & FAQs',
+    //   link: '/help-faqs',
+    //   type: 'secondary',
+    //   icon: 'i-material-symbols-contact-support-outline',
+    // },
+  ],
+}
